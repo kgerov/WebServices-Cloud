114d04dae931ca555dd8624fd39de62bfc9ca052
kgerov <kgerov@abv.bg>
2015-04-03 20:32:26 +0300

    Calc Service finished

---
+++ 01.SOA-REST/SOAP/CalculatorClient/CalculatorClient/CalculatorService.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculatorService
+{
+    public class CalculatorService : ICalculator
+    {
+        public int Multi(int numA, int numB)
+        {
+            return numA*numB;
+        }
+
+        public double CalcDistance(Point startPoint, Point endPoint)
+        {
+            double powDeltaX = Math.Pow((startPoint.X - endPoint.X), 2);
+            double powDeltaY = Math.Pow((startPoint.Y - endPoint.Y), 2);
+
+            return Math.Sqrt(powDeltaX + powDeltaY);
+        }
+    }
+}
---
+++ 01.SOA-REST/SOAP/CalculatorClient/CalculatorClient/ICalculator.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace CalculatorService
+{
+    [ServiceContract]
+    public interface ICalculator
+    {
+
+        [OperationContract]
+        int Multi(int numA, int numB);
+
+        [OperationContract]
+        double CalcDistance(Point startPoint, Point endPoint);
+    }
+
+
+    [DataContract]
+    public class Point
+    {
+        [DataMember]
+        public int X { get; set; }
+        [DataMember]
+        public int Y { get; set; }
+    }
+}
